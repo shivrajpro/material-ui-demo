c6c7aec6732ad41acc5bb163bd46a985a6d07687
shivrajpro <shivrajhulbatte@gmail.com>
2024-02-14 16:38:06 +0530

    create breakpoints to show feed only in extra small devices

--- src/App.js
+++ src/App.js
@@ -1,88 +1,21 @@
-import DeleteIcon from "@mui/icons-material/Delete";
-import SendIcon from "@mui/icons-material/Send";
-import { Button, styled } from "@mui/material";
-import { Typography } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import "./App.css";
-import { theme } from "./theme";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  const BlueButton = styled(Button)(({ theme }) => ({
-    backgroundColor: "dodgerblue",
-    color: "white",
-    margin: 5,
-    "&:hover": {
-      backgroundColor: "skyblue",
-    },
-    "&:disabled": {
-      backgroundColor: "gray",
-      color: "white",
-    },
-  }));
   return (
     <div>
-      <p>app works!</p>
-      <div>
-        <Button variant="text">text</Button>
-        <Button variant="contained" color="secondary">
-          contained
-        </Button>
-        <Button variant="outlined">outlined</Button>
-      </div>
-      <br />
-      <div>
-        <Button variant="outlined" startIcon={<DeleteIcon />}>
-          Delete
-        </Button>
-        <Button variant="contained" endIcon={<SendIcon />}>
-          Send
-        </Button>
-      </div>
-      <div>
-        <p>Typography</p>
-        <Typography variant="h1" component="h1">
-          h1. Heading
-        </Typography>
-        <Typography variant="h1" component="p">
-          it uses h1 style but its a p tag
-        </Typography>
-
-        <Button
-          variant="contained"
-          disabled
-          sx={{
-            backgroundColor: "dodgerblue",
-            color: "white",
-            margin: 5,
-            "&:hover": {
-              backgroundColor: "skyblue",
-            },
-            "&:disabled": {
-              backgroundColor: "gray",
-              color: "white",
-            },
-          }}
-        >
-          my disabled button
-        </Button>
-        <Button
-          variant="contained"
-          sx={{
-            backgroundColor: "dodgerblue",
-            color: "white",
-            margin: 5,
-            "&:hover": {
-              backgroundColor: "skyblue",
-            },
-            "&:disabled": {
-              backgroundColor: "gray",
-              color: "white",
-            },
-          }}
-        >
-          my unique button
-        </Button>
-        <BlueButton>my blue button</BlueButton>
-      </div>
+      {/* <p>app works!</p> */}
+      <Box>
+      {/* navbar */}
+      <Stack direction="row" spacing={2} justifyContent="space-between" >
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+      </Box>
     </div>
   );
 }
