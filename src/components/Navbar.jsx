8e50a734d9c4ae83c9529b978a069eed74546b0f
shivrajpro <shivrajhulbatte@gmail.com>
2024-02-15 12:01:58 +0530

    add icons in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,76 @@
-import { AppBar, Toolbar, Typography, styled } from "@mui/material";
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
 import React from "react";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between",
 });
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: { //600px
+    display: "none",
+  },
+}));
+
 const Navbar = () => {
   return (
     <AppBar position="sticky">
       <StyledToolbar>
-        <Typography>Navbar</Typography>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          LAMA DEV
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }}></Pets>
+        <Search>
+          <InputBase placeholder="search" />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail color="action" />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications color="action" />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+        </Icons>
+        <UserBox>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+          <Typography>John</Typography>
+        </UserBox>
       </StyledToolbar>
     </AppBar>
   );
