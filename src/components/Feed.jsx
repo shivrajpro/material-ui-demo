1fa2c62d5ebb31b92d936e23ed136e4ec744a866
shivrajpro <shivrajhulbatte@gmail.com>
2024-02-15 15:39:07 +0530

    complete feed and get started with rightbar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,16 @@
 import { Box } from "@mui/material";
 import React from "react";
+import Post from "./Post";
 
 const Feed = () => {
-  return <Box bgcolor="pink" flex={4} p={2}
-  >Feed</Box>;
+  return (
+    <Box flex={4} p={2}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </Box>
+  );
 };
 
 export default Feed;
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,17 +1,60 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
 
 const Rightbar = () => {
   return (
-    <Box bgcolor="lightcoral" flex={2} p={2}
-    sx={{
-      display: {
-        xs: "none",
-        sm: "block",
-      },
-    }}
+    <Box
+      flex={2}
+      p={2}
+      sx={{
+        display: {
+          xs: "none",
+          sm: "block",
+        },
+      }}
     >
-      Rightbar
+      <Box position="fixed">
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={7}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://material-ui.com/static/images/avatar/1.jpg"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://material-ui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://material-ui.com/static/images/avatar/3.jpg"
+          />
+          <Avatar alt="Agnes Walker" src="" />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/6.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/7.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/8.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/7.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/8.jpg"
+          />
+        </AvatarGroup>
+      </Box>
     </Box>
   );
 };
