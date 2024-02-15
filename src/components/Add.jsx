c058e68712b9be06f0794efec82a247f91301114
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2024-02-15 22:43:45 +0530

    add modal for create post

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,35 +1,29 @@
-import {
-    Add as AddIcon
-} from "@mui/icons-material";
-import {
-    Fab,
-    Modal,
-    styled,
-    Tooltip
-} from "@mui/material";
-import { Box } from "@mui/system";
+import { Add as AddIcon } from "@mui/icons-material";
+import { Avatar, Fab, TextField, Tooltip, Typography, styled } from "@mui/material";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 import React, { useState } from "react";
 
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
 const Add = () => {
-  const SytledModal = styled(Modal)({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  });
-
-  const UserBox = styled(Box)({
-    display: "flex",
-    alignItems: "center",
-    gap: "10px",
-    marginBottom: "20px",
-  });
-
   const [open, setOpen] = useState(false);
-
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <>
       <Tooltip
-        onClick={(e) => setOpen(true)}
+        onClick={handleOpen}
         title="Add"
         sx={{ position: "fixed", bottom: 20, left: { xs: "calc(50% - 25px)" } }}
       >
@@ -37,6 +31,35 @@ const Add = () => {
           <AddIcon />
         </Fab>
       </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} p={3} bgcolor="white" borderRadius={5}>
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create Post
+          </Typography>
+          <UserBox>
+            <Avatar
+              src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography fontWeight={500} variant="span">
+              John Doe
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{width:"100%"}}
+            id="standard-multiline-static"
+            multiline
+            rows={3}
+            variant="standard"
+            placeholder="what's on your mind?"
+          />
+        </Box>
+      </StyledModal>
     </>
   );
 };
